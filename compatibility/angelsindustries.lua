952d332ec119706510a976efdaa1ec622b254274
DerPumu <derpumu@users.noreply.github.com>
2024-06-25 18:19:00 +0200

    remove warfare blocks, treat all exploration blocks

--- compatibility/angelsindustries.lua
+++ compatibility/angelsindustries.lua
@@ -6,12 +6,22 @@ local update_exploration = function()
         array.remove(exploration_tech.prerequisites, "angels-components-weapons-basic")
     end
 
-    exploration_block1_recipe = data.raw.recipe["block-exploration-1"]
-    if exploration_block1_recipe then
-        for _, ingredient in pairs(exploration_block1_recipe.ingredients) do
-            if ingredient.name == "weapon-parts" then
-                ingredient.amount = ingredient.amount * 2
-                ingredient.name = "angels-gear"
+    local exploration_blocks = {
+        "block-exploration-1",
+        "block-exploration-2",
+        "block-exploration-3",
+        "block-exploration-4",
+        "block-exploration-5",
+    }
+
+    for _, block_name in pairs(exploration_blocks) do
+        exploration_block_recipe = data.raw.recipe[block_name]
+        if exploration_block_recipe then
+            for _, ingredient in pairs(exploration_block_recipe.ingredients) do
+                if ingredient.name == "weapon-parts" then
+                    ingredient.amount = ingredient.amount * 2
+                    ingredient.name = "mechanical-parts"
+                end
             end
         end
     end
@@ -21,15 +31,26 @@ return {
     --exceptions = {
     --    gun = "dolphin-gun"
     --},
-    --extra = {
-    --    misc = {
-    --        { "research-achievement", "destroyer-of-worlds" },
-    --        { "tips-and-tricks-item", "kr-creep" },
-    --        { "tips-and-tricks-item", "kr-new-gun-play" }
-    --    },
-    --    item = { "biters-research-data", "biomass" },
-    --    entity = { "kr-bio-lab" }
-    --},
+    extra = {
+        --    misc = {
+        --        { "research-achievement", "destroyer-of-worlds" },
+        --        { "tips-and-tricks-item", "kr-creep" },
+        --        { "tips-and-tricks-item", "kr-new-gun-play" }
+        --    },
+        item = {
+            -- "weapon-parts",
+            "angels-trigger",
+            "body-1",
+            "weapon-1",
+            "block-warfare-1",
+            "block-warfare-2",
+            "block-warfare-3",
+            "block-warfare-4",
+            "block-warfare-5",
+        },
+        entity = { "angels-war-lab-1" },
+        entity_types = { "lab" },
+    },
     --ignore = {
     --    result_items = { "kr-void", "matter" }
     --},
