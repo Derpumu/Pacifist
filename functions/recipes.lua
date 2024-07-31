2c080da39eccc4374706694d5bce28e60bea999c
DerPumu <derpumu@users.noreply.github.com>
2024-07-31 17:15:20 +0200

    refactor recipe processing

--- functions/recipes.lua
+++ functions/recipes.lua
@@ -1,20 +1,18 @@
 local array = require("__Pacifist__.lib.array")
 local data_raw = require("__Pacifist__.lib.data_raw")
 
-
--- recipes that generate any of the given items
-local function find_recipes_for(resulting_items)
-    local function is_military_result(entry)
+local function _is_military_recipe(recipe)
+    local function _is_military_result(entry)
         local name = (type(entry) == "string" and entry) or entry.name or entry[1]
-        return name and array.contains(resulting_items, name)
+        return name and array.contains(PacifistMod.military_item_names, name)
     end
 
-    local function contains_result(section)
+    local function _contains_military_result(section)
         if section.result then
-            return is_military_result(section.result)
+            return _is_military_result(section.result)
         elseif section.results then
             for _, result_entry in pairs(section.results) do
-                if is_military_result(result_entry) then
+                if _is_military_result(result_entry) then
                     return true
                 end
             end
@@ -24,22 +22,23 @@ local function find_recipes_for(resulting_items)
         end
     end
 
-    local function is_relevant_recipe(recipe)
-        return contains_result(recipe)
-                or (recipe.normal and contains_result(recipe.normal))
-                or (recipe.expensive and contains_result(recipe.expensive))
-    end
+    return _contains_military_result(recipe)
+            or (recipe.normal and _contains_military_result(recipe.normal))
+            or (recipe.expensive and _contains_military_result(recipe.expensive))
+end
 
+-- recipes that generate military items
+local function _find_military_recipes()
     local relevant_recipes = {}
     for _, recipe in pairs(data.raw.recipe) do
-        if is_relevant_recipe(recipe) then
+        if _is_military_recipe(recipe) then
             table.insert(relevant_recipes, recipe.name)
         end
     end
     return relevant_recipes
 end
 
-local function has_no_ingredients(recipe)
+local function _has_no_ingredients(recipe)
     if recipe.ingredients then
         return array.is_empty(recipe.ingredients)
     end
@@ -47,7 +46,7 @@ local function has_no_ingredients(recipe)
             or (recipe.expensive and array.is_empty(recipe.expensive.ingredients or {}))
 end
 
-local function is_ignored_result(result)
+local function _is_ignored_result(result)
     if type(result) == "string" then
         return not array.contains(PacifistMod.ignore.result_items, result)
     else
@@ -55,14 +54,14 @@ local function is_ignored_result(result)
     end
 end
 
-local function has_no_results(recipe)
+local function _has_no_results(recipe)
     local function section_has_no_result(section)
         if not section then return false end
 
         if section.result then
-            return is_ignored_result(section.result)
+            return _is_ignored_result(section.result)
         elseif section.results then
-            return array.all_of(section.results, is_ignored_result)
+            return array.all_of(section.results, _is_ignored_result)
         else
             return false
         end
@@ -74,45 +73,78 @@ local function has_no_results(recipe)
             or array.any_of(PacifistMod.ignore.recipe_pred, function(predicate) return predicate(recipe) end)
 end
 
-local function remove_military_recipe_ingredients(military_item_names, military_item_recipes)
-    local obsolete_recipes = {}
-    for _, recipe in pairs(data.raw.recipe) do
-        if not has_no_ingredients(recipe) then
-            local removed = {}
-            local function is_ingredient_military_item(ingredient)
-                -- ingredients have either the format {"advanced-circuit", 5}
-                -- or {type="fluid", name="water", amount=50}
-                local ingredient_name = ingredient.name or ingredient[1]
-                if array.contains(military_item_names, ingredient_name) then
-                    table.insert(removed, ingredient_name)
-                    return true
-                end
-                return false
-            end
+local function _is_ingredient_military_item(ingredient)
+    -- ingredients have either the format {"advanced-circuit", 5}
+    -- or {type="fluid", name="water", amount=50}
+    local ingredient_name = ingredient.name or ingredient[1]
+    if array.contains(PacifistMod.military_item_names, ingredient_name) then
+        return true
+    end
+    return false
+end
 
-            array.remove_in_place(recipe.ingredients, is_ingredient_military_item)
-            array.remove_in_place(recipe.normal and recipe.normal.ingredients, is_ingredient_military_item)
-            array.remove_in_place(recipe.expensive and recipe.expensive.ingredients, is_ingredient_military_item)
-            if (has_no_ingredients(recipe)) and has_no_results(recipe) then
-                table.insert(obsolete_recipes, recipe.name)
-            elseif (not array.is_empty(removed)) and (not array.contains(military_item_recipes, recipe.name)) then
-                log("removing ingredient(s) " .. array.to_string(removed) .. " from recipe " .. recipe.name)
-            end
+local function _has_only_military_ingredients(recipe)
+    if not recipe.normal and not recipe.expensive then
+        -- if neither normal nor expensive is defined, recipe.ingredients has to be defined
+        return not array.is_empty(recipe.ingredients)
+                and array.all_of(recipe.ingredients, _is_ingredient_military_item)
+    end
+
+    if recipe.normal
+            and (array.is_empty(recipe.normal.ingredients)
+            or not array.all_of(recipe.normal.ingredients, _is_ingredient_military_item)) then
+        return false
+    end
+
+    if recipe.expensive
+            and (array.is_empty(recipe.expensive.ingredients)
+            or not array.all_of(recipe.expensive.ingredients, _is_ingredient_military_item)) then
+        return false
+    end
+    return true
+end
+
+local function _replace_military_ingredients(recipe)
+    -- 1: find recipes that have military ingredients but do not produce military results
+    -- 2: log their ingredients as non_military, find recipes that make them
+    -- 3: check if those recipes have military ingredients (recursively)
+
+    local removed_ingredients = {}
+    local function _check_and_log(ingredient)
+        if _is_ingredient_military_item(ingredient) then
+            array.append_unique(removed_ingredients, { ingredient.name or ingredient[1] })
+            return true
+        else
+            return false
         end
     end
-    if (not array.is_empty(obsolete_recipes)) then
-        debug_log("removing recipes with no ingredients and no results left: " .. array.to_string(obsolete_recipes, "\n    "))
+
+    local function _remove_from_section(section)
+        if not section then return end
+        array.remove_in_place(section.ingredients, _check_and_log)
     end
-    return obsolete_recipes
+
+    if not recipe.normal and not recipe.expensive then
+        _remove_from_section(recipe)
+    end
+    if recipe.normal then _remove_from_section(recipe.normal) end
+    if recipe.expensive then _remove_from_section(recipe.expensive) end
+
+    if not array.is_empty(removed_ingredients) and not recipe.hidden then
+        log("Removed ingredients from recipe " .. recipe.name .. ": " .. array.to_string(removed_ingredients))
+    end
 end
 
 function PacifistMod.process_recipes()
-    local military_item_recipes = find_recipes_for(PacifistMod.military_item_names)
-
-    local more_obsolete_recipes = remove_military_recipe_ingredients(PacifistMod.military_item_names, military_item_recipes)
-    array.append(military_item_recipes, more_obsolete_recipes)
-
-    PacifistMod.military_recipes = military_item_recipes
+    local obsolete_recipes = {}
+    for _, recipe in pairs(data.raw.recipe) do
+        if _is_military_recipe(recipe) or _has_only_military_ingredients(recipe) then
+            table.insert(obsolete_recipes, recipe.name)
+        else
+            _replace_military_ingredients(recipe)
+        end
+    end
+    PacifistMod.military_recipes = obsolete_recipes
 end
 
 function PacifistMod.remove_recipes()
