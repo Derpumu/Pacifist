3d15c0a5c45ed76b0fdc671f47d8e480b0ff1484
DerPumu <derpumu@users.noreply.github.com>
2025-03-26 20:41:16 +0100

    fix locale issue

--- compatibility/SpidertronPatrols.lua
+++ compatibility/SpidertronPatrols.lua
@@ -1,6 +1,6 @@
 local clone_spidertron_dock_corpse = function()
     local cloned_corpse = table.deepcopy(data.raw.corpse["artillery-turret-remnants"])
-    cloned_corpse.name = "sp-spidertron-dock-corpse"
+    cloned_corpse.name = "sp-spidertron-dock-remnants"
 
     data.raw["proxy-container"]["sp-spidertron-dock"].corpse = cloned_corpse.name
     data:extend{cloned_corpse}
