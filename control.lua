68b15138f38b3b6ab2be14578ea3c15a2917711e
DerPumu <derpumu@users.noreply.github.com>
2025-04-03 18:51:05 +0200

    Fix that autoplace controls for "enemy-base" would be removed on init

--- control.lua
+++ control.lua
@@ -35,10 +35,6 @@ local function disable_enemies()
     game.forces.enemy.reset_evolution()
     for _, surface in pairs(game.surfaces) do
         find_and_destroy_enemies(surface)
-        local map_gen_settings = surface.map_gen_settings
-        map_gen_settings.autoplace_controls = map_gen_settings.autoplace_controls or {}
-        map_gen_settings.autoplace_controls["enemy-base"] = { size = "none", frequency = "none", richness = "none" }
-        surface.map_gen_settings = map_gen_settings
     end
     game.map_settings.enemy_expansion.enabled = false
     game.map_settings.enemy_evolution.enabled = false
