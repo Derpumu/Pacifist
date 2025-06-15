20134c7181f2f7a06a107b11568dcc226e95fbb4
DerPumu <derpumu@users.noreply.github.com>
2025-06-15 15:27:42 +0200

    add compatibility for pyrawores

--- compatibility/pycoalprocessing.lua
+++ compatibility/pycoalprocessing.lua
@@ -1,22 +1,36 @@
 require("__Pacifist__.functions.pacify_item")
+local array = require("__Pacifist__.lib.array") --[[@as Array]]
 
-local _handle_utility_science_capsules = function()
+local _pacify_gunpowder = function()
+    pacify_item("gunpowder", "grenade")
+end
+
+local _pacify_utility_science_capsules = function()
     pacify_item("destroyer-capsule", "utility-science-pack")
     pacify_item("distractor-capsule", "destroyer-capsule")
     pacify_item("defender-capsule", "distractor-capsule")
     pacify_item("piercing-rounds-magazine", "defender-capsule")
     pacify_item("firearm-magazine", "piercing-rounds-magazine")
-    pacify_item("gunpowder", "firearm-magazine")
 end
 
-return {
+local config = {
     extra = {
-        technology = {
-            "military",
-            "military-2",  -- together with the grenade changes in base.lua we removed the only military effects
-            "distractor",
-            "destroyer",
-        }
+        technology = {},
     },
-    preprocess = { _handle_utility_science_capsules }
+    preprocess = { _pacify_gunpowder }
 }
+
+-- pycoalprocessing changes the production chain for utility science to include all the capsules
+-- unless pyhightech is included as well, which changes the chain again
+if not mods["pyhightech"] then
+    table.insert(config.preprocess, _pacify_utility_science_capsules)
+    array.append(config.extra.technology, {
+        "military",
+        "military-2",     -- together with the grenade changes in base.lua we removed the only military effects
+        "distractor",
+        "destroyer",
+    })
+end
+
+
+return config
