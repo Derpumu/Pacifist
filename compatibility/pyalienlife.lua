d20441605d90cb52d2eacf5daa47b5cb07703b43
DerPumu <derpumu@users.noreply.github.com>
2025-06-16 18:14:35 +0200

    add comptibility to pyalternativeenergy

--- compatibility/pyalienlife.lua
+++ compatibility/pyalienlife.lua
@@ -1,4 +1,6 @@
 local recipes = require("__Pacifist__.functions.recipes")
+local items = require("__Pacifist__.functions.items")
+local technologies = require("__Pacifist__.functions.technologies")
 
 local _remove_shield_from_spidertron = function()
     if not settings.startup["pacifist-remove-shields"].value then return end
@@ -27,10 +29,21 @@ end
 local _convert_laser_turrets = function()
     local laser_turret_item = data.raw.item["laser-turret"]
     laser_turret_item.place_result = nil
+
+    local laser_equipment_item = data.raw.item["personal-laser-defense-equipment"]
+    laser_equipment_item.place_as_equipment_result = nil
 end
 
 local _remove_gun_turret_from_zungror_lair = function()
     recipes.remove_ingredient(data.raw, "zungror-lair-mk01", "gun-turret")
+end
+
+local _pacify_poison_capsule = function ()
+    local item = items.change_type(data.raw, "poison-capsule", "item")
+    local cage = items.find(data.raw, "cage")
+    item.subgroup = cage.subgroup
+    item.order = (cage.order or "").."z"
+    technologies.move_unlock(data.raw, "poison-capsule", "military-3", "bioreactor-upgrade")
 end
 
 local config = {
@@ -39,15 +52,24 @@ local config = {
             "chorkok", "gobachov", "huzu",
             "caravan", "flyavan", "caravan-turd", "flyavan-turd",
             "digosaurus", "thikat", "work-o-dile", "digosaurus-turd", "thikat-turd", "work-o-dile-turd",
-            "ocula"
+            "ocula",
+            "aerial-blimp-mk01", "aerial-blimp-mk02", "aerial-blimp-mk03", "aerial-blimp-mk04",
         },
         technology = {
             "arthurian-upgrade",
             "compost-upgrade",
             "domestication-mk04",
+            "genlab-upgrade",
+            "ralesia-upgrade",
         },
     },
-    preprocess = { _remove_shield_from_spidertron, _fix_car_corpses, _convert_laser_turrets, _remove_gun_turret_from_zungror_lair },
+    preprocess = {
+        _remove_shield_from_spidertron,
+        _fix_car_corpses,
+        _convert_laser_turrets,
+        _remove_gun_turret_from_zungror_lair,
+        _pacify_poison_capsule,
+    },
 }
 
 return config
