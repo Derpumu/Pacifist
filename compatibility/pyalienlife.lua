b4c685065db7ed191323fba0ae01b3220879f633
DerPumu <derpumu@users.noreply.github.com>
2025-06-17 08:52:57 +0200

    adapt technologies and prerequisites for Py compatibility

--- compatibility/pyalienlife.lua
+++ compatibility/pyalienlife.lua
@@ -26,24 +26,39 @@ local _fix_car_corpses = function()
     data.raw["spider-vehicle"]["phadaisus"].corpse = big_corpse_name
 end
 
-local _convert_laser_turrets = function()
-    local laser_turret_item = data.raw.item["laser-turret"]
-    laser_turret_item.place_result = nil
-
+local _convert_military_turd_ingredients = function()
     local laser_equipment_item = data.raw.item["personal-laser-defense-equipment"]
     laser_equipment_item.place_as_equipment_result = nil
+    laser_equipment_item.localised_name = { "item-name.pacifist-py-genlab-laser" }
+    laser_equipment_item.subgroup = "py-alienlife-genetics"
+    laser_equipment_item.order = "zz"
+    technologies.move_unlock(data.raw, "personal-laser-defense-equipment", "personal-laser-defense-equipment",
+        "genlab-upgrade")
+
+    local laser_euipment_recipe = data.raw.recipe["personal-laser-defense-equipment"]
+    laser_euipment_recipe.localised_name = { "item-name.pacifist-py-genlab-laser" }
+
+    local laser_turret_item = data.raw.item["laser-turret"]
+    laser_turret_item.localised_name = { "item-name.pacifist-py-biotech-laser" }
+    laser_turret_item.place_result = nil
+    laser_turret_item.subgroup = "py-alienlife-items"
+    laser_turret_item.order = "aza"
+
+    local laser_turret_recipe = data.raw.recipe["laser-turret"]
+    laser_turret_recipe.localised_name = { "item-name.pacifist-py-biotech-laser" }
+
+    local laser_turret_tech = data.raw.technology["laser-turret"]
+    laser_turret_tech.localised_name = { "item-name.pacifist-py-biotech-laser" }
+    laser_turret_tech.localised_description = { "", "" }
+
+    local item = items.change_type(data.raw, "poison-capsule", "item")
+    item.subgroup = "py-alienlife-items"
+    item.order = "azb"
+    technologies.move_unlock(data.raw, "poison-capsule", "military-3", "bioreactor-upgrade")
 end
 
 local _remove_gun_turret_from_zungror_lair = function()
     recipes.remove_ingredient(data.raw, "zungror-lair-mk01", "gun-turret")
-end
-
-local _pacify_poison_capsule = function ()
-    local item = items.change_type(data.raw, "poison-capsule", "item")
-    local cage = items.find(data.raw, "cage")
-    item.subgroup = cage.subgroup
-    item.order = (cage.order or "").."z"
-    technologies.move_unlock(data.raw, "poison-capsule", "military-3", "bioreactor-upgrade")
 end
 
 local config = {
@@ -63,12 +78,16 @@ local config = {
             "ralesia-upgrade",
         },
     },
+    extra = {
+        technology = {
+            "personal-laser-defense-equipment"
+        },
+    },
     preprocess = {
         _remove_shield_from_spidertron,
         _fix_car_corpses,
-        _convert_laser_turrets,
+        _convert_military_turd_ingredients,
         _remove_gun_turret_from_zungror_lair,
-        _pacify_poison_capsule,
     },
 }
 
