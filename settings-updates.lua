05a5dce5b135200ffb1c0b8d291f0b1f4bb8e471
DerPumu <derpumu@users.noreply.github.com>
2025-07-27 13:48:05 +0200

    change default setting for shield removal for space age

--- settings-updates.lua
+++ settings-updates.lua
@@ -19,6 +19,23 @@ local function force_setting(name, value)
     end
 end
 
+local function default_setting(name, value)
+    if data.raw["bool-setting"] and data.raw["bool-setting"][name] then
+        local the_setting = data.raw["bool-setting"][name]
+        the_setting.default_value = value
+        return
+    end
+
+    for _, type in pairs({"int-setting", "double-setting", "string-setting"}) do
+        local category = data.raw[type] or {}
+        local the_setting = category[name]
+        if the_setting then
+            the_setting.default_value = value
+            return
+        end
+    end
+end
+
 if mods["blueprint-shotgun"] then
     -- blueprint shotgun's default recipe uses an actual shotgun, this setting changes that
     force_setting("blueprint-shotgun-no-wood", true)
@@ -26,4 +43,9 @@ end
 
 if mods["pyalienlife"] then
     force_setting("pacifist-remove-walls", false)
+end
+
+if mods["space-age"] then
+    -- Fulgora without shields is a challenge...
+    default_setting("pacifist-remove-shields", false)
 end
