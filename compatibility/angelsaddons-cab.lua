e8e1b17316a9320b3735edeff79f007dccad2421
DerPumu <derpumu@users.noreply.github.com>
2026-03-08 13:13:59 +0100

    add Angel's mods compatibility

---
+++ compatibility/angelsaddons-cab.lua
@@ -0,0 +1,16 @@
+return {
+    exceptions = {
+        ammo = {
+            "angels-cab-deploy-charge",
+            "angels-cab-undeploy-charge"
+        },
+        ammo_category = {
+            "angels-cab-deploy",
+            "angels-cab-undeploy",
+        },
+        gun = {
+            "angels-cab-deploy",
+            "angels-cab-undeploy",
+        },
+    }
+}
