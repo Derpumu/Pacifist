4aec8dae540552a265b8f36af53ad9fc06fee893
DerPumu <derpumu@users.noreply.github.com>
2026-03-10 17:49:56 +0100

    fix periodic madness compatibility oversights

--- compatibility/periodic-madness.lua
+++ compatibility/periodic-madness.lua
@@ -4,14 +4,19 @@ local _remove_achievements = function()
     data.raw["use-item-achievement"]["pm-use-more-gun"] = nil
 end
 
-local _move_cyano_recipe = function()
+local _move_recipes = function()
     technologies.move_unlock(data.raw, "pm-cyanobacteriaed-agar-solution", "pm-chemical-weaponry",
         "pm-agar-bacteria-filtering")
+    technologies.move_unlock(data.raw, "pm-water-hydrogen-peroxide", "rocketry", "explosives")
 end
 
 return {
-    preprocess = { _remove_achievements, _move_cyano_recipe },
+    preprocess = {
+        _remove_achievements,
+        _move_recipes,
+    },
     extra = {
-        fluid = { "pm-phosgene", "pm-melatonin" }
+        fluid = { "pm-phosgene", "pm-melatonin" },
+        item = { "grenade" },
     }
 }
